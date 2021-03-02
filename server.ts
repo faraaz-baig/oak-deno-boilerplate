d9e1d9e0e5ecdc4fb82c0fcc9b67c84852ab48c2
faraaz baig <fazzy0908@yandex.com>
2021-03-03 01:41:17 +0530

    initial commit

---
+++ server.ts
@@ -0,0 +1,24 @@
+import {
+  Application,
+  Router,
+  RouterContext,
+} from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+const router = new Router();
+
+router.get("/", (ctx: RouterContext) => {
+  ctx.response.body = "hello world, Faraaz here";
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods()); // telling router to just use the method allowed to run on it for example: "/" route can only run ger method as we have configured it in our code
+
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+  console.log(
+    `listening on ${secure ? "https://" : "http://"}${hostname ||
+      "localhost"}:${port}`,
+  );
+});
+
+await app.listen({ port: 8000 });
